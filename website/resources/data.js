056fb515959270c737b92e915b19e17f84581a12
condorhauck <jon@jonblackburn.com>
2014-11-21 09:48:22 -0500

    Added Files for Demo
    Updated the demo to include the code referenced for the demo

---
+++ website/resources/data.js
@@ -0,0 +1,38 @@
+﻿var liveColors = [
+                {
+                    "bdpt": "head", "color": "#fff", "title": "The Head",
+                    "data": "The upper or anterior division of the animal body that contains the brain, the chief sense organs, and the mouth."
+                },
+                {
+                    "bdpt": "torso", "color": "#fff", "title": "The Torso",
+                    "data": "The main part of the human body not including the head, arms, and legs."
+                },
+                {
+                    "bdpt": "legs", "color": "#fff", "title": "The Legs",
+                    "data": "The long body parts that are used especially for standing, walking, and running"
+                },
+                {
+                    "bdpt": "right-foot", "color": "#fff", "title": "The Right Foot",
+                    "data": "The part of the leg, on the right-side of the body, on which an animal or person stands and moves : the part of the leg below the ankle"
+                },
+                {
+                    "bdpt": "left-foot", "color": "#fff", "title": "The Left Foot",
+                    "data": "The part of the leg, on the left-side of the body, on which an animal or person stands and moves : the part of the leg below the ankle"
+                },
+                {
+                    "bdpt": "right-arm", "color": "#fff", "title": "The Right Arm",
+                    "data": "A human upper limb, on the right side of the body; especially :  the part between the shoulder and the wrist"
+                },
+                {
+                    "bdpt": "left-arm", "color": "#fff", "title": "The Left Arm",
+                    "data": "A human upper limb, on the left side of the body; especially :  the part between the shoulder and the wrist"
+                },
+                {
+                    "bdpt": "right-hand", "color": "#fff", "title": "The Right Hand",
+                    "data": "The body part at the end of your right arm that includes your fingers and thumb"
+                },
+                {
+                    "bdpt": "left-hand", "color": "#fff", "title": "The Left Hand",
+                    "data": "The body part at the end of your left arm that includes your fingers and thumb"
+                },
+]
